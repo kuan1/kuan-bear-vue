eadf99aef93d3f41efcd816660799f0eff0996de
luzhongkuan <luzhongk@126.com>
2018-09-12 23:15:27 +0800

    pagination

--- src/packages/autoAudio/AutoAudio.vue
+++ src/packages/autoAudio/AutoAudio.vue
@@ -1,9 +1,15 @@
 <template>
-  <audio v-if="src" :src="src" autoplay></audio>
+  <audio v-if="src" :src="src" autoplay="autoplay"></audio>
 </template>
 
 <script>
   export default {
+    props: {
+      autoplay: {
+        type: Boolean,
+        default: true
+      }
+    },
     data() {
       return {
         visible: false,
---
+++ src/test/TestPagination.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <my-pagination :total="800" :page.sync="page"></my-pagination>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        page: 7
+      }
+    }
+  }
+</script>
