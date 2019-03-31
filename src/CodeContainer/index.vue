ab638320d763c4e43cccfe7c836693b1bda798fa
luzhongkuan <luzhongk@126.com>
2019-03-31 12:35:52 +0800

    udpate

---
+++ src/CodeContainer/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="code" class="code" v-html="html"></div>
+</template>
+
+<script>
+import hljs from 'highlight.js'
+import 'highlight.js/styles/androidstudio.css'
+
+export default {
+  props: {
+    html: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  watch: {
+    html() {
+      this.init()
+    }
+  },
+  methods: {
+    init() {
+      const blocks = this.$refs.code.querySelectorAll('pre code');
+      blocks.forEach((block) => {
+        hljs.highlightBlock(block)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.code {
+  line-height: 1.2;
+}
+</style>
---
+++ test/view/codeContainer/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <code-container :html="html" />
+</template>
+
+<script>
+import CodeContainer from '../../../src/CodeContainer'
+
+export default {
+  components: {
+    CodeContainer
+  },
+  data() {
+    return {
+      html: `
+  <pre>
+    <code>
+      const fs = require('fs')
+      const axios = require('axios')
+      const cheerio = require('cheerio')
+
+      const baseUrl = 'https://pixabay.com'
+
+      let page = 1
+
+      const url = 'https://pixabay.com/images/search/nature/?cat=science'
+    </code>
+  </pre>
+`
+    }
+  }
+}
+</script>
+
+<style>
+</style>
